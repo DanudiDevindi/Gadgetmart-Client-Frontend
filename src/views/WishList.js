58a6efa58c30537b5898c02fabcb91f49cffab08
Danudi <danusilva1997@gmail.com>
2022-01-24 13:31:07 +0530

    wishlist

---
+++ src/views/WishList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Headers from '../containers/default-layout/Header';
+import Cookies from "js-cookie";
+
+class WishList extends React.Component {
+
+}
+
+export default WishList;
