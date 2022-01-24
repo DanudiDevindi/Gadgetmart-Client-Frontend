9fee8f2899ef881f79c842142802e4fc845bcb90
Danudi <danusilva1997@gmail.com>
2022-01-24 14:02:24 +0530

    wishlist ui

--- src/views/WishList.js
+++ src/views/WishList.js
@@ -4,6 +4,50 @@ import Cookies from "js-cookie";
 
 class WishList extends React.Component {
 
+    render() {
+        let cart = Cookies.get('WishList');
+        if (cart === undefined) {
+            cart = [];
+        } else {
+            cart = JSON.parse(cart);
+        }
+        return (
+            <div>
+                <Headers history={this.props.history}/>
+                <main className="main">
+                    <nav aria-label="breadcrumb" className="breadcrumb-nav">
+                        <div className="container">
+                            <ol className="breadcrumb marginTop">
+                                <li className="breadcrumb-item"><a onClick={() => this.props.history.push('/')}><i
+                                    className="icon-home"/></a>
+                                </li>
+                                <li className="breadcrumb-item active" aria-current="page">Wish List</li>
+                            </ol>
+                        </div>
+                    </nav>
+
+                    <div className="container">
+                        <div className="row">
+                            {
+                                cart.map((item, index) => {
+                                    return (
+                                        <Item
+                                            key={index}
+                                            item={item}
+                                            onAdd={()=>this.setState({added:true})}
+                                            cookies={Cookies}
+                                            props={this.props}/>
+                                    );
+                                })
+                            }
+
+                        </div>
+                    </div>
+                </main>
+            </div>
+        )
+    }
+
 }
 
 export default WishList;
