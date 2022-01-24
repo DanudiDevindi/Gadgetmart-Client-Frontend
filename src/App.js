127f831aa7b44e3afcd7c329d7d321c2b3459824
Danudi <danusilva1997@gmail.com>
2022-01-24 13:29:51 +0530

    wishlist routings

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './views/Home';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import AboutUs from './views/AboutUs';
 import Product from './views/Product';
+import WishList from "./views/WishList";
 
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
           <Route exact path="/" name="Home Page" component={Home} />
           <Route exact path="/aboutUs" name="About Us" component={AboutUs} />
           <Route exact path="/product" name="Product" component={Product} />
+          <Route exact path="/wishList" name="Wish List" component={WishList}/>
         </Switch>
       </BrowserRouter>
     );
