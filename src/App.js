9ceb823c694a6c72134f34ee01a8fcd5ec95f060
Danudi <danusilva1997@gmail.com>
2022-01-24 12:02:30 +0530

    product ui

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './views/Home';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import AboutUs from './views/AboutUs';
+import Product from './views/Product';
 
 class App extends Component {
 
@@ -13,6 +14,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" name="Home Page" component={Home} />
           <Route exact path="/aboutUs" name="About Us" component={AboutUs} />
+          <Route exact path="/product" name="Product" component={Product} />
         </Switch>
       </BrowserRouter>
     );
